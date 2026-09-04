bda53e6734a20c8a6bf2c61ddf6b0c8835f52c35
beesani1992 <beesani1992@gmail.com>
2026-09-04 22:00:02 +0500

    Update authMiddleware.jschanges

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = 'your-secret-key-123';
+const JWT_SECRET = '29963ae672c4af92ef6c298a1509fa01bcf42f520a4205230dba314ae751a61a';
 
 export function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
