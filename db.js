53ae190a0762322875b3b21ff1bf43186ec6892b
beesani1992 <beesani1992@gmail.com>
2026-09-05 23:19:00 +0500

    Updated5 db.js

--- db.js
+++ db.js
@@ -4,11 +4,18 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// Log during startup to see what Vercel is actually passing
-console.log('Connecting to DB Host:', process.env.DATABASE_URL ? process.env.DATABASE_URL.split('@')[1] : 'DATABASE_URL IS UNDEFINED');
+const connectionString = process.env.DATABASE_URL;
+
+// Debug log: Prints host portion without exposing password
+if (connectionString) {
+  const hostPart = connectionString.split('@')[1] || 'invalid-format';
+  console.log('ACTIVE DB HOST:', hostPart);
+} else {
+  console.log('ACTIVE DB HOST: DATABASE_URL IS UNDEFINED');
+}
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: { rejectUnauthorized: false }
 });
 
