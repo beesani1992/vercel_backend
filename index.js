93eeb34b22f64a10cdf7618489a1abf1180e4c93
beesani1992 <beesani1992@gmail.com>
2026-09-12 23:09:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// api/index.js
-import express from 'express';
-import cors from 'cors';
-import paymentRoutes from './payments.js';
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// Mount routes
-app.use('/payments', paymentRoutes);
-
-export default app;
